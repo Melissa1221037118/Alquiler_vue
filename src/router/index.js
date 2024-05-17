49944f640336aa7cdb26aa3c3b4969128d8046f6
Melissa Montaño vallecilla <melissavallecilla0821@gmail.com>
2024-05-17 14:19:31 -0500

    Routers vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+import EditarCliente from '../components/Cliente/EditarCliente.vue';
+import NewCliente from '../components/Cliente/NewCliente.vue';
+import Clientes from '../views/Clientes.vue';
+
+import EditarVehiculo from '../components/Vehiculo/EditarVehiculo.vue';
+import NewVehiculo from '../components/Vehiculo/NewVehiculo.vue';
+import Vehiculos from '../views/Vehiculos.vue';
+
+import EditarContratoDeAlquiler from '../components/ContratoDeAlquiler/EditarContratoDeAlquiler.vue';
+import NewContratoDeAlquiler from '../components/ContratoDeAlquiler/NewContratoDeAlquiler.vue';
+import ContratosDeAlquiler from '../views/ContratosDeAlquiler.vue';
+
+import EditarPago from '../components/Pago/EditarPago.vue';
+import NewPago from '../components/Pago/NewPago.vue';
+import Pagos from '../views/Pagos.vue';
+
+import EditarReserva from '../components/Reserva/EditarReserva.vue';
+import NewReserva from '../components/Reserva/NewReserva.vue';
+import Reservas from '../views/Reservas.vue';
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -8,6 +29,81 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/clientes',
+    name: 'clientes',
+    component: Clientes
+  },
+  {
+    path: '/editar-cliente/:id',
+    name: 'EditarCliente',
+    component: EditarCliente
+  },
+  {
+    path: '/add-cliente/',
+    name: 'NewCliente',
+    component: NewCliente
+  },
+  {
+    path: '/vehiculos',
+    name: 'vehiculos',
+    component: Vehiculos
+  },
+  {
+    path: '/editar-vehiculo/:id',
+    name: 'EditarVehiculo',
+    component: EditarVehiculo
+  },
+  {
+    path: '/add-vehiculo/',
+    name: 'NewVehiculo',
+    component: NewVehiculo
+  },
+  {
+    path: '/contratos_de_alquiler',
+    name: 'contratosDeAlquiler',
+    component: ContratosDeAlquiler
+  },
+  {
+    path: '/editar-contrato-de-alquiler/:id',
+    name: 'EditarContratoDeAlquiler',
+    component: EditarContratoDeAlquiler
+  },
+  {
+    path: '/add-contrato-de-alquiler/',
+    name: 'NewContratoDeAlquiler',
+    component: NewContratoDeAlquiler
+  },
+  {
+    path: '/pagos',
+    name: 'pagos',
+    component: Pagos
+  },
+  {
+    path: '/editar-pago/:id',
+    name: 'EditarPago',
+    component: EditarPago
+  },
+  {
+    path: '/add-pago/',
+    name: 'NewPago',
+    component: NewPago
+  },
+  {
+    path: '/reservas',
+    name: 'reservas',
+    component: Reservas
+  },
+  {
+    path: '/editar-reserva/:id',
+    name: 'EditarReserva',
+    component: EditarReserva
+  },
+  {
+    path: '/add-reserva/',
+    name: 'NewReserva',
+    component: NewReserva
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
@@ -15,7 +111,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
